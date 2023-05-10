ad6f34e0ffe87606e4e7a979c7051bb9aca2c74f
Alvian istiqlal hanafi <alvianhanafi17@gmail.com>
2023-05-10 08:27:42 +0700

    Feat index

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const usersRoutes = require('./routes/users');
+const categoriesRoutes = require('./routes/categories');
+const recipeRoutes = require('./routes/recipes');
 const response = require('./utils/respons');
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use('/users', usersRoutes);
+app.use('/categories', categoriesRoutes);
+app.use('/recipes', recipeRoutes);
 app.use((req, res) => {
   response(404, null, 'Endpoint not found', res);
 });
