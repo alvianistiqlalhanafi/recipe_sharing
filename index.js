8b1a47d0238cf8a97b841a6ac3c7177d2fecc437
Alvian istiqlal hanafi <alvianhanafi17@gmail.com>
2023-05-13 08:27:53 +0700

    feat : update index

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const usersRoutes = require('./routes/users');
 const categoriesRoutes = require('./routes/categories');
 const recipeRoutes = require('./routes/recipes');
+const reviewRoutes = require('./routes/reviews');
 const response = require('./utils/respons');
 const PORT = process.env.PORT || 5000;
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use('/users', usersRoutes);
 app.use('/categories', categoriesRoutes);
 app.use('/recipes', recipeRoutes);
+app.use('/reviews', reviewRoutes);
 app.use((req, res) => {
   response(404, null, 'Endpoint not found', res);
 });
